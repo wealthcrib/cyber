7a6b4d1ce135db3c3328be777ebc7e8af20b8600
wealthcrib <eagleajanaku1@gmail.com>
2023-09-14 12:13:51 +0100

    assignment1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstname =Console.Readline();
+        Console.Write("What is your last name? ");
+        string lastname = Console.Readline();
+        Console.WriteLine($"Your name is {lastname} ,{firstname} {lastname} .");
     }
 }
